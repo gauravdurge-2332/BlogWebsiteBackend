f049a8f8683ed62927e664bd78332e9e4df32105
Gaurav Durge <[durgegaurav2332@gmail.com]>
2026-05-16 12:27:53 +0530

    Made the Api views for the Category and the Tag

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,9 @@ from django.urls import path , include
 from .views import * 
 
 urlpatterns = [
-    path("" , Postapiview.as_view() , name='getAllpost') 
+    path("" , Postapiview.as_view() , name='getAllpost') ,
+    path('catg/' ,CategoryApiview.as_view() , name='tempdatapost' ),
+    path('tags/' ,TagApiView.as_view() , name='tagkapost' )
+
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -12,3 +12,53 @@ class Postapiview(APIView):
         post = Post.objects.all().order_by('created_at')
         serializer = PostSerializer(post , many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def post(self,request): 
+        serailzer = PostSerializer(data = request.data) 
+
+        if serailzer.is_valid() :
+            serailzer.save() 
+
+            return Response(serailzer.data) 
+        
+        return Response({'Error' : 'There is the error'})
+            
+class CategoryApiview(APIView):
+
+    def get(self, request):
+
+        data = Category.objects.all()
+
+        serializer = CategorySerializer(data, many=True)
+
+        return Response(serializer.data)
+         
+
+
+    def post(self , request):
+            serailizer = CategorySerializer(data = request.data)
+            if serailizer.is_valid() :
+                serailizer.save() 
+
+                return Response({"result" : 'Response Posted successfully'})
+    
+            return Response({'result' : 'Error in posting data'})
+    
+
+class TagApiView(APIView):
+     
+     def post(self , request):
+          data = TagSerializer(data = request.data , many=True) 
+
+          if data.is_valid() : 
+               data.save() 
+
+               return Response({'result' : 'Data posted successfully'})
+          return Response({'result' : 'Error in posting the data'})
+     
+     def get(self , request): 
+          data = Tags.objects.all() 
+          serailizer = TagSerializer(data,many = True) 
+
+          return Response(serailizer.data)
+    
